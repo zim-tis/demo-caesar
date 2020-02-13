b04f8b94502b6e68992329e37e8d327fff013774
Stéphane Zimmermann <stephane.zimmermann@trust-in-soft.com>
2020-02-13 13:53:51 +0100

    "commit43"

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main(void)
     int str_len = sizeof orig_str;
 
     printf("Test 1: Shift with a negative input\n");
-    gen_test(orig_str, str_len, 171754816);
+    gen_test(orig_str, str_len, 31625303);
     printf("\nTest 2: Shift with a positive input\n");
     gen_test(orig_str, str_len, 7);
 
